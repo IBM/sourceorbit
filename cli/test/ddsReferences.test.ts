7922ce73c1885cc30df20ae09a55082c0a777d4b
worksofliam <mrliamallan@live.co.uk>
2024-02-16 16:00:33 -0500

    Tests for usecase without refs file
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- cli/test/ddsReferences.test.ts
+++ cli/test/ddsReferences.test.ts
@@ -7,6 +7,7 @@ import { getFiles } from '../src/utils';
 import { setupFixture } from './fixtures/projects';
 import { scanGlob } from '../src/extensions';
 import { writeFileSync } from 'fs';
+import { BobProject } from '../src';
 
 const cwd = setupFixture(`dds_refs`);
 
@@ -16,8 +17,8 @@ let files = getFiles(cwd, scanGlob);
 
 describe.skipIf(files.length === 0)(`dds_refs tests`, () => {
   const targets = new Targets(cwd);
-  targets.setSuggestions({renames: true, includes: true})
-  
+  targets.setSuggestions({ renames: true, includes: true })
+
   beforeAll(async () => {
     targets.loadObjectsFromPaths(files);
     const parsePromises = files.map(f => targets.parseFile(f));
@@ -32,16 +33,16 @@ describe.skipIf(files.length === 0)(`dds_refs tests`, () => {
     expect(targets.getResolvedObjects(`FILE`).length).toBe(3);
     expect(targets.binderRequired()).toBe(false);
 
-    const pro250d = targets.searchForObject({systemName: `PRO250D`, type: `FILE`});
+    const pro250d = targets.searchForObject({ systemName: `PRO250D`, type: `FILE` });
     expect(pro250d).toBeDefined();
-    const provider = targets.searchForObject({systemName: `PROVIDER`, type: `FILE`});
+    const provider = targets.searchForObject({ systemName: `PROVIDER`, type: `FILE` });
     expect(provider).toBeDefined();
-    const provide1 = targets.searchForObject({systemName: `PROVIDE1`, type: `FILE`});
+    const provide1 = targets.searchForObject({ systemName: `PROVIDE1`, type: `FILE` });
     expect(provide1).toBeDefined();
   });
 
   test(`test PROD250D deps (REF & 32REFFLD)`, async () => {
-    const pro250d = targets.getTarget({systemName: `PRO250D`, type: `FILE`});
+    const pro250d = targets.getTarget({ systemName: `PRO250D`, type: `FILE` });
 
     expect(pro250d).toBeDefined();
     const deps = pro250d.deps;
@@ -58,7 +59,7 @@ describe.skipIf(files.length === 0)(`dds_refs tests`, () => {
   });
 
   test(`test PROVIDER deps (REF)`, async () => {
-    const provider = targets.getTarget({systemName: `PROVIDER`, type: `FILE`});
+    const provider = targets.getTarget({ systemName: `PROVIDER`, type: `FILE` });
 
     expect(provider).toBeDefined();
     const deps = provider.deps;
@@ -74,7 +75,7 @@ describe.skipIf(files.length === 0)(`dds_refs tests`, () => {
   });
 
   test(`test PROVIDE1 deps (REF)`, async () => {
-    const providerLf = targets.getTarget({systemName: `PROVIDE1`, type: `FILE`});
+    const providerLf = targets.getTarget({ systemName: `PROVIDE1`, type: `FILE` });
 
     expect(providerLf).toBeDefined();
     const deps = providerLf.deps;
@@ -82,5 +83,28 @@ describe.skipIf(files.length === 0)(`dds_refs tests`, () => {
 
     const logs = targets.logger.getLogsFor(providerLf.relativePath);
     expect(logs).toBeUndefined();
+  });
+
+  test(`make doesn't include refs that do not exist`, () => {
+    const project = new MakeProject(cwd, targets);
+
+    const targetContent = project.generateTargets();
+
+    expect(targetContent).toContain(`$(PREPATH)/PROVIDE1.FILE: $(PREPATH)/PROVIDER.FILE`);
+    expect(targetContent).toContain(`$(PREPATH)/PRO250D.FILE: $(PREPATH)/PROVIDER.FILE`);
+  });
+
+  test(`bob doesn't include refs that do not exist`, () => {
+    const project = new BobProject(targets);
+
+    const files = project.createRules();
+
+    expect(files[`Rules.mk`]).toBeDefined();
+
+    const baseRules = files[`Rules.mk`].split('\n').map(l => l.trim());
+
+    expect(baseRules).toContain(`PRO250D.FILE: PRO250D.DSPF PROVIDER.FILE`);
+    expect(baseRules).toContain(`PROVIDER.FILE: PROVIDER.PF`);
+    expect(baseRules).toContain(`PROVIDE1.FILE: PROVIDE1.LF PROVIDER.FILE`);
   });
 });
