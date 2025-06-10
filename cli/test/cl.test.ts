a42b656acfd867d4bb189a0a71101abdf68f3bac
worksofliam <mrliamallan@live.co.uk>
2025-06-10 17:16:29 -0400

    Test case for bug CL parser not handling brackets correctly #124
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

---
+++ cli/test/cl.test.ts
@@ -0,0 +1,20 @@
+import { beforeAll, describe, expect, test } from 'vitest';
+import { fromCl, toCl } from '../src/utils';
+
+describe(`CL parsing / generating`, () => {
+  test('can parse & generate correctly', () => {
+    const input = `CRTSQLRPGI OBJ(SNDBX87/NEWEMP) SRCSTMF('qrpglesrc/newemp.pgm.sqlrpgle') OPTION(*EVENTF) DBGVIEW(*SOURCE) CLOSQLCSR(*ENDMOD) CVTCCSID(*JOB) COMPILEOPT('TGTCCSID(*JOB)') RPGPPOPT(*LVL2)`;
+
+    const { command, parameters } = fromCl(input);
+
+    expect(command).toBe('CRTSQLRPGI');
+    expect(Object.keys(parameters)).toHaveLength(8);
+
+    console.log(parameters);
+    expect(parameters[`compileopt`]).toBe(`'TGTCCSID(*JOB)'`);
+
+    const generated = toCl(command, parameters);
+
+    expect(generated).toBe(input);
+  });
+});
